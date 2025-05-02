714d13ae6c282d9f6477e7ef4d86c120ff79e6df
y <yqwx01z@gmail.com>
2025-05-02 18:08:02 +0200

    commit

--- frontend/app.py
+++ frontend/app.py
@@ -403,6 +403,55 @@ def docker_api(req_method,req_var):
 
 
 
+def gpu_to_pd():
+    global MEM_TOTAL
+    global MEM_USED
+    global MEM_FREE
+    rows = []
+
+    try:
+        gpu_list = get_gpu_data()
+        MEM_TOTAL = 0
+        MEM_USED = 0
+        MEM_FREE = 0
+        for entry in gpu_list:
+            gpu_info = ast.literal_eval(entry['gpu_info'])
+            
+            current_gpu_mem_total = gpu_info.get("mem_total", "0")
+            current_gpu_mem_used = gpu_info.get("mem_used", "0")
+            current_gpu_mem_free = gpu_info.get("mem_free", "0")
+            MEM_TOTAL = float(MEM_TOTAL) + float(current_gpu_mem_total.split()[0])
+            MEM_USED = float(MEM_USED) + float(current_gpu_mem_used.split()[0])
+            MEM_FREE = float(MEM_FREE) + float(current_gpu_mem_free.split()[0])
+
+            
+            rows.append({                                
+                "name": gpu_info.get("name", "0"),
+                "mem_util": gpu_info.get("mem_util", "0"),
+                "timestamp": entry.get("timestamp", "0"),
+                "fan_speed": gpu_info.get("fan_speed", "0"),
+                "temperature": gpu_info.get("temperature", "0"),
+                "gpu_util": gpu_info.get("gpu_util", "0"),
+                "power_usage": gpu_info.get("power_usage", "0"),
+                "clock_info_graphics": gpu_info.get("clock_info_graphics", "0"),
+                "clock_info_mem": gpu_info.get("clock_info_mem", "0"),                
+                "cuda_cores": gpu_info.get("cuda_cores", "0"),
+                "compute_capability": gpu_info.get("compute_capability", "0"),
+                "current_uuid": gpu_info.get("current_uuid", "0"),
+                "gpu_i": entry.get("gpu_i", "0"),
+                "supported": gpu_info.get("supported", "0"),
+                "not_supported": gpu_info.get("not_supported", "0"),
+                "status": "ok"
+            })
+
+        df = pd.DataFrame(rows)
+        return df
+    
+    except Exception as e:
+        print(f'[{datetime.now().strftime("%Y-%m-%d %H:%M:%S")}] {e}')
+
+gpu_to_pd()
+
 
 
 
@@ -3365,8 +3414,8 @@ def create_app():
         mhm_timer = gr.Timer(0.1,active=True)
         mhm_timer.tick(update_gpu_data, outputs=[kekwq])
         
-        # gpu_timer = gr.Timer(1,active=True)
-        # gpu_timer.tick(gpu_to_pd, outputs=gpu_dataframe)
+        gpu_timer = gr.Timer(1,active=True)
+        gpu_timer.tick(gpu_to_pd, outputs=gpu_dataframe)
 
         network_timer = gr.Timer(1,active=True)
         network_timer.tick(network_to_pd, outputs=[network_dataframe,kekw])
